69a289d84c958a915bde0ab3692543cb81708aab
Gigi Minova <minova1995@gmail.com>
2024-03-05 02:32:46 +0900

    add nav

--- src/routes/Information.jsx
+++ src/routes/Information.jsx
@@ -1,7 +1,10 @@
+import FixedBottomNavigation from '../components/Navigation';
+
 export default function Information() {
   return (
     <div>
       <h1>Information</h1>
+      <FixedBottomNavigation />
     </div>
   );
 }
