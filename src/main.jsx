e0c889b7582c1eef20d9e8c1f1ec057e5f626c10
Gigi Minova <minova1995@gmail.com>
2024-03-21 05:44:55 +0900

    Add recoil root

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { RecoilRoot } from 'recoil';
 import { Auth0Provider } from '@auth0/auth0-react';
 import { ErrorBoundary } from 'react-error-boundary';
 import App from './App.jsx';
@@ -8,15 +9,17 @@ import './index.css';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ErrorBoundary fallback={<ErrorBoundary />}>
-      <Auth0Provider
-        domain="dev-wfjw7s3fxbkrb37k.jp.auth0.com"
-        clientId="RNtJJDAOoOX3PUB5lpW9dKqrr5K4jVfH"
-        authorizationParams={{
-          redirect_uri: window.location.origin,
-        }}
-      >
-        <App />
-      </Auth0Provider>
+      <RecoilRoot>
+        <Auth0Provider
+          domain="dev-wfjw7s3fxbkrb37k.jp.auth0.com"
+          clientId="RNtJJDAOoOX3PUB5lpW9dKqrr5K4jVfH"
+          authorizationParams={{
+            redirect_uri: window.location.origin,
+          }}
+        >
+          <App />
+        </Auth0Provider>
+      </RecoilRoot>
     </ErrorBoundary>
   </React.StrictMode>,
 );
