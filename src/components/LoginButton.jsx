d7955cb294e77c5b09f62f22da7a407fcb8eebcd
Gigi Minova <minova1995@gmail.com>
2024-03-21 02:14:26 +0900

    style button

--- src/components/LoginButton.jsx
+++ src/components/LoginButton.jsx
@@ -3,7 +3,21 @@ import { useAuth0 } from '@auth0/auth0-react';
 const LoginButton = () => {
   const { loginWithRedirect } = useAuth0();
 
-  return <button onClick={() => loginWithRedirect()}>ログイン</button>;
+  const ButtonStyle = {
+    color: 'white',
+    border: 'none',
+    borderRadius: '30px',
+    backgroundColor: '#EB6159',
+    height: '60px',
+    width: '265px',
+    margin: '0 auto',
+  };
+
+  return (
+    <button style={ButtonStyle} onClick={() => loginWithRedirect()}>
+      ログイン
+    </button>
+  );
 };
 
 export default LoginButton;
